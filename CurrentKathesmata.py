12881462cbe51e9bd8e76f60641eb4990751f798
Danielsccc <140761607+Danielsccc@users.noreply.github.com>
2023-08-08 11:45:36 -0400

    Add files via upload

--- CurrentKathesmata.py
+++ CurrentKathesmata.py
@@ -1,16 +1,207 @@
 import time
 
 current_time = time.localtime()
-print(current_time)
-print(current_time.tm_hour)
-print(current_time.tm_wday)
 
+def read_psalm(filename):
+    print(f"Psalm {filename.strip('src/.txt')}")
+    file = open(filename, 'r')
+    print(file.read())
+    file.close()
 
-if current_time.tm_hour >= 8 and current_time.tm_hour < 19: #checks whether it is evening or it is morning
-    matins()
-else:
-    vespers()
+def first_kathisma():
+    read_psalm("src/001.txt")
+    read_psalm("src/002.txt")
+    read_psalm("src/003.txt")
+    read_psalm("src/004.txt")
+    read_psalm("src/005.txt")
+    read_psalm("src/006.txt")
+    read_psalm("src/007.txt")
+    read_psalm("src/008.txt")
+    
+def second_kathisma():
+    read_psalm("src/009.txt")
+    read_psalm("src/010.txt")
+    read_psalm("src/011.txt")
+    read_psalm("src/012.txt")
+    read_psalm("src/013.txt")
+    read_psalm("src/014.txt")
+    read_psalm("src/015.txt")
+    read_psalm("src/016.txt")
 
+def third_kathisma():
+    read_psalm("src/017.txt")
+    read_psalm("src/018.txt")
+    read_psalm("src/019.txt")
+    read_psalm("src/020.txt")
+    read_psalm("src/021.txt")
+    read_psalm("src/022.txt")
+    read_psalm("src/023.txt")
+
+def fourth_kathisma():
+    read_psalm("src/024.txt")
+    read_psalm("src/025.txt")
+    read_psalm("src/026.txt")
+    read_psalm("src/027.txt")
+    read_psalm("src/028.txt")
+    read_psalm("src/029.txt")
+    read_psalm("src/030.txt")
+    read_psalm("src/031.txt")
+    
+def fifth_kathisma():
+    read_psalm("src/032.txt")
+    read_psalm("src/033.txt")
+    read_psalm("src/034.txt")
+    read_psalm("src/035.txt")
+    read_psalm("src/036.txt")
+
+def sixth_kathisma():
+    read_psalm("src/037.txt")
+    read_psalm("src/038.txt")
+    read_psalm("src/039.txt")
+    read_psalm("src/040.txt")
+    read_psalm("src/041.txt")
+    read_psalm("src/042.txt")
+    read_psalm("src/043.txt")
+    read_psalm("src/044.txt")
+    read_psalm("src/045.txt")
+
+def seventh_kathisma():
+    read_psalm("src/046.txt")
+    read_psalm("src/047.txt")
+    read_psalm("src/048.txt")
+    read_psalm("src/049.txt")
+    read_psalm("src/050.txt")
+    read_psalm("src/051.txt")
+    read_psalm("src/052.txt")
+    read_psalm("src/053.txt")
+    read_psalm("src/054.txt")
+
+def eighth_kathisma():
+    read_psalm("src/055.txt")
+    read_psalm("src/056.txt")
+    read_psalm("src/057.txt")
+    read_psalm("src/058.txt")
+    read_psalm("src/059.txt")
+    read_psalm("src/060.txt")
+    read_psalm("src/061.txt")
+    read_psalm("src/062.txt")
+    read_psalm("src/063.txt")
+
+def ninth_kathisma():
+    read_psalm("src/064.txt")
+    read_psalm("src/065.txt")
+    read_psalm("src/066.txt")
+    read_psalm("src/067.txt")
+    read_psalm("src/068.txt")
+    read_psalm("src/069.txt")
+
+def tenth_kathisma():
+    read_psalm("src/070.txt")
+    read_psalm("src/071.txt")
+    read_psalm("src/072.txt")
+    read_psalm("src/073.txt")
+    read_psalm("src/074.txt")
+    read_psalm("src/075.txt")
+    read_psalm("src/076.txt")
+
+def eleventh_kathisma():
+    read_psalm("src/077.txt")
+    read_psalm("src/078.txt")
+    read_psalm("src/079.txt")
+    read_psalm("src/080.txt")
+    read_psalm("src/081.txt")
+    read_psalm("src/082.txt")
+    read_psalm("src/083.txt")
+    read_psalm("src/084.txt")
+
+def twelth_kathisma():
+    read_psalm("src/085.txt")
+    read_psalm("src/086.txt")
+    read_psalm("src/087.txt")
+    read_psalm("src/088.txt")
+    read_psalm("src/089.txt")
+    read_psalm("src/090.txt")
+
+def thirteenth_kathisma():
+    read_psalm("src/091.txt")
+    read_psalm("src/092.txt")
+    read_psalm("src/093.txt")
+    read_psalm("src/094.txt")
+    read_psalm("src/095.txt")
+    read_psalm("src/096.txt")
+    read_psalm("src/097.txt")
+    read_psalm("src/098.txt")
+    read_psalm("src/099.txt")
+    read_psalm("src/100.txt")
+
+def forteenth_kathisma():
+    read_psalm("src/101.txt")
+    read_psalm("src/102.txt")
+    read_psalm("src/103.txt")
+    read_psalm("src/104.txt")
+
+def fifteenth_kathisma():
+    read_psalm("src/105.txt")
+    read_psalm("src/106.txt")
+    read_psalm("src/107.txt")
+    read_psalm("src/108.txt")
+
+def sixteenth_kathisma():
+    read_psalm("src/109.txt")
+    read_psalm("src/110.txt")
+    read_psalm("src/111.txt")
+    read_psalm("src/112.txt")
+    read_psalm("src/113.txt")
+    read_psalm("src/114.txt")
+    read_psalm("src/115.txt")
+    read_psalm("src/116.txt")
+    read_psalm("src/117.txt")
+
+def seventeenth_kathisma():
+    read_psalm("src/118.txt")
+
+def eighteenth_kathisma():
+    read_psalm("src/119.txt")
+    read_psalm("src/120.txt")
+    read_psalm("src/121.txt")
+    read_psalm("src/122.txt")
+    read_psalm("src/123.txt")
+    read_psalm("src/124.txt")
+    read_psalm("src/125.txt")
+    read_psalm("src/126.txt")
+    read_psalm("src/127.txt")
+    read_psalm("src/128.txt")
+    read_psalm("src/129.txt")
+    read_psalm("src/130.txt")
+    read_psalm("src/131.txt")
+    read_psalm("src/132.txt")
+    read_psalm("src/133.txt")
+
+def ninteenth_kathisma():
+    read_psalm("src/134.txt")
+    read_psalm("src/135.txt")
+    read_psalm("src/136.txt")
+    read_psalm("src/137.txt")
+    read_psalm("src/139.txt")
+    read_psalm("src/140.txt")
+    read_psalm("src/141.txt")
+    read_psalm("src/142.txt")
+
+def twentith_kathisma():
+    read_psalm("src/143.txt")
+    read_psalm("src/144.txt")
+    read_psalm("src/145.txt")
+    read_psalm("src/146.txt")
+    read_psalm("src/147.txt")
+    read_psalm("src/148.txt")
+    read_psalm("src/149.txt")
+    read_psalm("src/150.txt")
+
+def no_kathisma():
+    for i in range(99):
+        print("O Lord Jesus Christ, have mercy on me, the sinner!")
+    for t in range(33):
+        print("Most Holy Theotokos, save us!")
 
 def matins(): #defines the early morning service
     if current_time.tm_wday == 0: #Monday
@@ -71,119 +262,7 @@ def vespers():  #defines the early evening service
     if current_time.tm_wday == 6: #Sunday
         no_kathisma()
 
-def first_kathisma():
-    file = open("src/001.txt", 'r')
-    print(file.read())
-    file.close()
-    
-    file = open("src/002.txt", 'r')
-    print(file.read())
-    file.close()
-    
-    file = open("src/003.txt", 'r')
-    print(file.read())
-    file.close()
-    
-    file = open("src/004.txt", 'r')
-    print(file.read())
-    file.close()
-    
-    file = open("src/005.txt", 'r')
-    print(file.read())
-    file.close()
-    
-    file = open("src/006.txt", 'r')
-    print(file.read())
-    file.close()
-    
-    file = open("src/007.txt", 'r')
-    print(file.read())
-    file.close()
-    
-    file = open("src/008.txt", 'r')
-    print(file.read())
-    file.close()
-    
-    file = open("src/009.txt", 'r')
-    print(file.read())
-    file.close()
-   
-    
-def second_kathisma():
-    pass
-
-def third_kathisma():
-    pass
-
-def fourth_kathisma():
-    pass
-
-def fifth_kathisma():
-    pass
-
-def sixth_kathisma():
-    pass
-
-def seventh_kathisma():
-    pass
-
-def eighth_kathisma():
-    pass
-
-def ninth_kathisma():
-    pass
-
-def tenth_kathisma():
-    pass
-
-def eleventh_kathisma():
-    pass
-
-def twelth_kathisma():
-    pass
-
-def thirteenth_kathisma():
-    pass
-
-def forteenth_kathisma():
-    pass
-
-def fifteenth_kathisma():
-    pass
-
-def sixteenth_kathisma():
-    pass
-
-def seventeenth_kathisma():
-    pass
-
-def eighteenth_kathisma():
-    pass
-
-def ninteenth_kathisma():
-    pass
-
-def twentith_kathisma():
-    pass
-
-def no_kathisma():
-    for i in range(99):
-        print("O Lord Jesus Christ, have mercy on me, the sinner!")
-
-
-
-
-def read_psalm(filename)
-
-
-
-
-
-'''
-try:
-    file = open("src/001.txt", 'r')
-    print(file.read())
-    file.close()
-except FileNotFoundError:
-    print("File not found.")
-'''
+if current_time.tm_hour >= 8 and current_time.tm_hour < 19: #checks whether it is evening or it is morning
+    matins()
+else:
+    vespers()
